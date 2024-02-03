b2d89512ab41601991369483394052c0603fabaa
shinyp <98683195+MONKEFORLIfE@users.noreply.github.com>
2024-02-04 00:50:49 +1300

    Initial commit

---
+++ src/main/java/net/vrishin/firstmod/FirstModClient.java
@@ -0,0 +1,11 @@
+package net.vrishin.firstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FirstModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
